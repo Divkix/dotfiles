05941ac0d05f4bc56113da48dddc2860d90f774a
Divanshu Chauhan <23524935+Divkix@users.noreply.github.com>
2021-09-19 17:01:48 +0530

    move pre-requisite stuff to a separate script

---
+++ scripts/initial_config.sh
@@ -0,0 +1,32 @@
+#! /usr/bin/env sh
+
+DIR=$(dirname "$0")
+cd "$DIR"
+
+. ../scripts/functions.sh
+
+info "Installing some pre-requisites..."
+
+substep_info "Installing Yay Package Manager..."
+if $i --version &>/dev/null; then
+    substep_success "Yay already installed."
+elif sudo pacman -S yay --noconfirm &>/dev/null; then
+    substep_success "Finished installing Yay package manager."
+else
+    substep_error "Failed to install Yay package manager."
+fi
+
+reqPackages=("base-devel")
+
+for i in "${reqPackages[@]}"; do
+  substep_info "Installing $i..."
+    if yay -Qs $i &>/dev/null; then
+        substep_success "$i already installed."
+    elif yay -S --noconfirm &>/dev/null; then
+        substep_success "Finished installing $i."
+    else
+        substep_error "Failed to install $i."
+    fi
+done
+
+success "Finished installing pre-requisites."
