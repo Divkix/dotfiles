0b312f566f5f23d55d141598aa092c3517fe4257
Divanshu Chauhan <23524935+Divkix@users.noreply.github.com>
2021-09-20 20:33:16 +0530

    fix path

--- fish/config.fish
+++ fish/config.fish
@@ -4,7 +4,7 @@
 eval (/home/linuxbrew/.linuxbrew/bin/brew shellenv)
 
 # Add python3 unversioned symlinks to path
-set -gx PATH "/home/linuxbrew/.linuxbrew/opt/python@3.9/libexec/bin" PATH
+set -gx PATH "/home/linuxbrew/.linuxbrew/opt/python@3.9/libexec/bin" $PATH
 
 # setup gpg tty
 set -gx GPG_TTY (tty)
