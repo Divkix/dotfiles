5b941c7e89a8ab55f769291abb44ad841af0e25f
Divanshu Chauhan <23524935+Divkix@users.noreply.github.com>
2021-10-20 19:33:44 +0530

    use another way of configuring WSL

--- fish/config.fish
+++ fish/config.fish
@@ -5,12 +5,6 @@ set -gx GPG_TTY (tty)
 # if yes, then add some things to PATH
 if string match -r -i -q "^(.+)-microsoft-standard-WSL2" -- (uname -r)
 
-    # Get windows user directory        
-    set WINDOWS_USER_NAME (string split -f2 -r -m1 \\ (/mnt/c/Windows/System32/cmd.exe /c echo %USERPROFILE% | string trim -c \r))
-
-    # clean mess made by cmd
-    clear
-
     # add vscode to path
     set -gx PATH "/mnt/c/Users/$WINDOWS_USER_NAME/scoop/apps/vscode/current/bin/" $PATH
 
--- wsl/setup.sh
+++ wsl/setup.sh
@@ -14,7 +14,9 @@ SOURCE_FILE="$SOURCE/$CONFIG_FILE"
 DESTINATION_FILE="$DESTINATION/$CONFIG_FILE"
 
 info "Setting up WSL config file..."
-
 scopy "$SOURCE_FILE" "$DESTINATION_FILE"
+
+info "Setting up fish shell for WSL..."
+fish -c 'set -Ux WINDOWS_USER_NAME (string split -f2 -r -m1 \\ (/mnt/c/Windows/System32/cmd.exe /c echo %USERPROFILE% | string trim -c \r))' &>/dev/null
 
 success "Finished configuring WSL"
