395c84adeaa2b78daf5667b846dc20256caca2c2
Divanshu Chauhan <divkix@divkix.me>
2025-07-07 17:01:14 -0700

    update: add OrbStack and LM Studio CLI configurations

--- fish/config.fish
+++ fish/config.fish
@@ -52,3 +52,8 @@ starship init fish | source
 # Added by OrbStack: command-line tools and integration
 # This won't be added again if you remove it.
 source ~/.orbstack/shell/init2.fish 2>/dev/null || :
+
+# Added by LM Studio CLI (lms)
+set -gx PATH $PATH /Users/divkix/.lmstudio/bin
+# End of LM Studio CLI section
+
